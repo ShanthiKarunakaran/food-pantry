86112d0a718931c2317dfb15a3a7c2b19a9dd50e
Shanthi Karunakaran <shanthi.arun@gmail.com>
2025-12-09 20:59:08 -0800

    Add configuration for database connection
    added: config file was missing in server/src folder

---
+++ server/src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    databaseUrl:
+      "postgresql://neondb_owner:npg_F5oVndBxJN4m@ep-mute-darkness-afb53sr9-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  };
+
+
+export default config;
